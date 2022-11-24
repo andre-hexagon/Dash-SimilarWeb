778ecbc1cf124b9e68788529e93e8508b28543a6
Gerardo Coss Alcantara <gerardo@m-360.io>
2022-11-24 12:22:26 -0600

    graficas funcionales

--- graficas.py
+++ graficas.py
@@ -2,13 +2,16 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
+import plotly.express as px
 import pandas as pd
 import numpy as np
+from PIL import Image
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 from nltk.corpus import stopwords
 stop = stopwords.words("spanish")
+
 
 ################################################## 
 ######### Gráficas Temporales (1 Curvas) #########
@@ -23,7 +26,7 @@ def TemporalGraph1(t,x1):
         title_text = "Lorem Ipsum Y",
         title_font = {"size": 20})
 
-    return(fig.show())
+    return(fig)
 
 ################################################## 
 ######### Gráficas Temporales (2 Curvas) #########
@@ -46,7 +49,7 @@ def TemporalGraph2(t,y1,y2):
         title_text = "Lorem Ipsum Y",
         title_font = {"size": 20})
 
-    return(fig.show())
+    return(fig)
 
 
 ################################################## 
@@ -68,27 +71,22 @@ def plot_wordcloud(df,category):
     text2_cp
     wc = WordCloud(background_color='white', width=1080, height=360)
     wc.generate(text2_cp)
-    return wc.to_image()
 
-################################################## 
-######### Nube de Palabras para barras #########
-##################################################
-def plot_wordcloud(df,category, w, mode = 'RGB',file_ouput = 'image.png'):
-    df = df[df.main_category.isnull() == False]
-    dfc = df[df['main_category'].str.contains(category)]
-    dfc['tokenized_title']=dfc['title'].str.split(" ",expand = False)
-    dfc['title']=dfc['title'].str.lower()
-    dfc['title_wo_stopwords']=dfc['title'].apply(lambda x:[item for item in str(x).split() if item not in stop])
-    dfc3p = dfc.loc[dfc.index.repeat(dfc.estimated_purchases)]
-    dfc3p = dfc3p.reset_index()
-    text_cp=[]
-    for i in range(0,len(dfc3p)):
-        text_cp += dfc3p.title_wo_stopwords[i]
-    text_cp
-    text1_cp= " ".join(text_cp)
-    text2_cp = text1_cp.replace(',','')
-    text2_cp
-    wc = WordCloud(mode=mode,width=w, height=210, random_state = 77300, 
-                            background_color = None, collocations=False).generate(text2_cp)
-    wc.to_file(file_ouput)
-    return wc.to_image()
+    img = wc.to_image()
+
+    # Convert the image string to numpy array and create a
+    # Plotly figure, see https://plotly.com/python/imshow/
+    fig = px.imshow(np.array(img))
+
+    # Hide the axes and the tooltips
+    fig.update_layout(
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        margin=dict(t=20, b=0, l=0, r=0),
+        xaxis=dict(
+            showgrid=False,
+            showticklabels=False,
+            linewidth=0
+        ))
+        
+    return(fig)
