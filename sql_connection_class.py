0400ec37b50a0a2d49c453a849bc90f6a5ebc959
DiegoHernandez <diego.hernandez@hexagondata.io>
2022-11-29 22:24:12 +0100

    connection when query

---
+++ sql_connection_class.py
@@ -0,0 +1,28 @@
+import mysql.connector
+import pandas as pd
+
+HOST = ''
+DATABASE = ''
+USER = ''
+PASSWORD = ''
+
+class sql_connection():
+    def __init__(self, user, pswd, host, db):
+        self.cnx = cnx = mysql.connector.connect(user=user, password=pswd, host=host, database=db, charset='latin1')
+        
+    def exec_query(self, query_str):
+        cursor = self.cnx.cursor()
+        cursor.execute(query_str)
+
+        df = pd.DataFrame(cursor.fetchall(), columns=cursor.column_names)
+        for col in df.columns[df.dtypes==object]:
+            df[col]=df[col].apply(lambda x: x.encode('latin1').decode())
+
+        return df
+#####
+# USE CASE
+#####
+#SQL = sql_connection(USER, PASSWORD, HOST, DATABASE)
+#query_str = "SELECT * FROM table LIMIT 100"
+#df = SQL.query_to_df(query_str)
+#####
