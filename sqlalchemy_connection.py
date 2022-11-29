ef39b2360810a00173149cfbe0ee719bb5fbca13
DiegoHernandez <diego.hernandez@hexagondata.io>
2022-11-29 19:46:14 +0100

    Add connection using sqlalchemy

--- sqlalchemy_connection.py
+++ sqlalchemy_connection.py
@@ -7,23 +7,20 @@ USER = ''
 PASSWORD = ''
 
 class sql_connection():
-    def __init__(self):
+    def __init__(self, user, pswd, host, db):
         self.engine = None
         try:
-            self.engine = sqlalchemy.create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}".format(user=USER,
-                                                                                            pw=PASSWORD,
-                                                                                            db=DATABASE,
-                                                                                            host=HOST)
-                                                    )
+            self.engine = sqlalchemy.create_engine(f"mysql+pymysql://{user}:{pswd}@{host}/{db}")
             self.connection = self.engine.connect()
-
+            print(f"Successfull connection to {host}/{DATABASE}")
         except:
             print("Error while connectingengine to SQL")
 
     def exec_query(self, query_str):
         return self.connection.execute(sqlalchemy.sql.text(query_str))
 
-    def query_to_df(self, query_result):
+    def query_to_df(self, query_str):
+        query_result = self.exec_query(query_str)
         df = pd.DataFrame(query_result.fetchall())
         if len(df) > 0:
             df.columns = query_result.keys()
@@ -36,7 +33,7 @@ class sql_connection():
 #####
 # USE CASE
 #####
-#SQL = sql_connection()
-#query = SQL.exec_query("SELECT * FROM table")
-#df = query_to_df(query)
+#SQL = sql_connection(USER, PASSWORD, HOST, DATABASE)
+#query_str = "SELECT * FROM table LIMIT 100"
+#df = SQL.query_to_df(query_str)
 #####
